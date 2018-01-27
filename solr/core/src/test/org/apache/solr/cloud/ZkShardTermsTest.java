96e8933c28ebf22ed8dc7f2e4d848531374a16aa
Cao Manh Dat <datcm@apache.org>
2018-01-27 12:34:36 +0700

    SOLR-11702: Fix precommit

--- solr/core/src/test/org/apache/solr/cloud/ZkShardTermsTest.java
+++ solr/core/src/test/org/apache/solr/cloud/ZkShardTermsTest.java
@@ -26,7 +26,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
-import java.util.Set;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicInteger;
